74a330ff4d6601800b48022be2b06be6127ebd98
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-03-18 17:30:30 +0000

    draft footer added

--- weather-forecast/src/App.js
+++ weather-forecast/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import NavigationBar from './components/Navbar';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
   <Router>
     <div>
       <NavigationBar />
+      <Footer />
     </div>
   </Router>
   )
