5d8ee18f2f31b048ade02667bf94a1dd582e55b7
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-03-18 17:29:00 +0000

    draft navbar added

--- weather-forecast/src/App.js
+++ weather-forecast/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
+import NavigationBar from './components/Navbar';
+
 export default function App() {
-  return (<div>
-    <h1>Hello World!</h1>
+  return (
+  <Router>
+    <div>
+      <NavigationBar />
     </div>
+  </Router>
   )
 }
---
+++ weather-forecast/src/components/Navbar.js
@@ -0,0 +1,8 @@
+
+export default function NavigationBar() {
+    return (
+        <div>
+        <h1> This is a Navigation bar</h1>
+        </div>
+    )
+}
