15175407ce425c840da8a5d327c9cde801e6b126
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-03-20 17:01:38 +0000

    basic css added to navbar

--- weather-forecast/src/components/Navbar.js
+++ weather-forecast/src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import "../style/Navbar.css"
+import Container from 'react-bootstrap/Container';
+import "../style/Navbar.css";
 
 export default function NavigationBar() {
     return (
         <div id="navbar">
-        <h1 id="title">Weather forecast app</h1>
+            <Container >
+                <div id="title">
+                    <h1>Weather forecast app</h1>
+                </div>
+            </Container>
         </div>
     )
 }
